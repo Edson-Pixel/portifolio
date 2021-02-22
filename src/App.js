5cdd1c6be3dcdfeba3295621984f00b6a859c9ba
Edson Araujo <araujoedson790@gmail.com>
2021-02-22 14:57:43 -0300

    Update App

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     <div className="background">
       
         <Navbar expand="md" className='background-menu' light>
-          <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHhcQs_Xhty6A/profile-displayphoto-shrink_100_100/0/1606271354952?e=1613606400&v=beta&t=ytLeE0QTRqdjA3zDQY9r9EeeY3DStykGZbQ7OQn-NyE" alt={'perfil-Edson'} className="perfil" />
+          <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHhcQs_Xhty6A/profile-displayphoto-shrink_100_100/0/1606271354952?e=1619654400&v=beta&t=JK2ihCyENDTzzDim0i6v1uHLbWCLUFvjKiopyv5LS2Q" alt={'perfil-Edson'} className="perfil" />
           <strong className='user'>Édson Araújo</strong>
           <NavbarToggler onClick={toggle} />
           <Collapse isOpen={isOpen} navbar >
